35facc3de19d4df38b0d169ded1062896e2486c3
JayCode Developer <JayCode.contakt@gmail.com>
2023-09-04 03:24:11 +0200

    Setup project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { run } from "./app/app"
 import "./styles/main.scss"
 
-console.log("inderbnx");
 run()
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
-const pages = ["index", "about"];
+const pages = ["index"];
 
 module.exports = {
     entry: "./src/index.js",
